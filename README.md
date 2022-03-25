eb9e6be4df9122e8086ab9269a7b1edf4b0f3f10
Tadesse Tilahun <ettad2@gmail.com>
2022-03-24 21:01:07 -0600

    Added some code to script.js to evaluate user input and generate password based on the criteria user selected.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# password-generator
-This project will generate random password as per user specifications
+# Password Generator
+
+## Purpose Of The Project
+
+This application enables users to generate random passwords based on criteria that they have selected. From the prompt window the user gets to choose if they want uppercase, lowercase, numbers, and special characters in their password. After answering those questions, the application generates a password and displays it on the page.
+
+
--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,71 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var numbers = "012345678";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var spacialChar="!@#$%^&*()_-";
+var temp = "";
+var password = "";
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 // Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
+function writePassword() 
+{
+  // var password = generatePassword();
   var passwordText = document.querySelector("#password");
+  passwordText.value="";
+  var passwordLength = prompt('How long is your password (8-128)?');
+  
+  if (passwordLength <8 || passwordLength > 128 )
+  {
+    console.log(passwordLength);
+    alert('Password lenght is not within range');
+    return writePassword();
+  };
+  
+  var inputCahr = confirm ("Do you want spacial characters?");
+  
+  
+  if(inputCahr)
+  {
+    temp += spacialChar;
+  };
+  
+  var inputLower = confirm ("Do you want lowercase?");
+  
+  if(inputLower == true)
+  {
+    temp += lowerCase;
+  };
+  
+  var inputUpper = confirm ("Do you want uppercase?");
+  
+  if(inputUpper == true)
+  {
+    temp += lowerCase.toUpperCase();
+  };
+  
+  var inputNum = confirm ("Do you want numbers?");
+
+    if(inputNum == true)
+    {
+      temp += numbers;
+    };
+
+    if (inputCahr == false && inputLower == false && inputUpper == false && inputNum == false)
+    {
+      alert("you must select one of the options")
+      return writePassword();
+    }
+  //generates the random password
+  for (let i = 0; i < passwordLength; i++) {
+    password += temp[Math.floor(Math.random()*temp.length)];
+    
+  }
+
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
